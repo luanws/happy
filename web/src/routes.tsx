3f87e10fce6d9d7823503626b39e1990147939d5
luanws <luan.w.silveira@gmail.com>
2020-10-15 18:31:40 -0300

    páginas de cadastro e detalhes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import CreateOrphanage from './pages/CreateOrphanage'
 import Landing from './pages/Landing'
+import Orphanage from './pages/Orphanage'
 import OrphanagesMap from './pages/OrphanagesMap'
 
 const Routes: React.FC = () => {
@@ -9,6 +11,8 @@ const Routes: React.FC = () => {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   )
