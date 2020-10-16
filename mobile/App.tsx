6dafd6e8a560147bdb2ce863e2ffbd9911b38c13
luanws <luan.w.silveira@gmail.com>
2020-10-16 14:37:05 -0300

    mapa

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+import mapMarker from './src/images/map-marker.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.mapView}
+        initialRegion={{
+          latitude: -27.2092052,
+          longitude: -49.6401092,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -27.2092052,
+            longitude: -49.6401092
+          }}
+          calloutAnchor={{ x: 2.4, y: 0.8 }}
+        >
+          <Callout tooltip onPress={() => { alert('oi') }}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Nome do orfanato</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +43,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  mapView: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+  calloutContainer: {
+    width: 160,
+    height: 55,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14
+  }
+})
